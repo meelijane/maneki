7ff6cf15edf63f7d1cfa630f8b6404f7e86203dc
Amelia Schmidt <amelia@godeldesign.com>
2013-02-16 17:55:23 +1100

    looking' nice

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// We assume that the
+// latest version of jQuery is bound to jQ and the
+// Drupal core version is bound to jQuery.
+(function(jQuery, $, window, undefined) {
+
+  // Document ready.
+  $(function(){
+      $.localScroll();
+  });
+
+  // Window load.
+  $(window).load(function(){
+
+  });
+})(jQuery, $, window);
+
+
